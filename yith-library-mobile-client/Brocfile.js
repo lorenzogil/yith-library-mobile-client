8ad39a9b3957edb9e918dc4b401a2ecc3b7f5c85
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-06-28 21:26:40 -0700

    Update to ember-cli 0.0.37

--- yith-library-mobile-client/Brocfile.js
+++ yith-library-mobile-client/Brocfile.js
@@ -4,19 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
 
-var app = new EmberApp({
-  name: require('./package.json').name,
-
-  // for some large projects, you may want to uncomment this (for now)
-  es3Safe: true,
-
-  minifyCSS: {
-    enabled: true,
-    options: {}
-  },
-
-  getEnvJSON: require('./config/environment')
-});
+var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -30,25 +18,6 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-
-app.import({
-  development: 'vendor/ember-data/ember-data.js',
-  production:  'vendor/ember-data/ember-data.prod.js'
-}, {
-  'ember-data': [
-    'default'
-  ]
-});
-
-app.import('vendor/ic-ajax/dist/named-amd/main.js', {
-  'ic-ajax': [
-    'default',
-    'defineFixture',
-    'lookupFixture',
-    'raw',
-    'request',
-  ]
-});
 
 // Ember IndexedDB Adapter
 app.import('vendor/ember-indexeddb-adapter/packages/indexeddb-adapter/lib/indexeddb_migration.js');
