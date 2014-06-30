2d47b1d1af389cd26fcb2c214fb9e37fa8322835
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-06-29 20:50:51 -0700

    Transition to secrets from the index route

--- yith-library-mobile-client/app/routes/index.js
+++ yith-library-mobile-client/app/routes/index.js
@@ -1,3 +1,9 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+
+    setupController: function () {
+        this.transitionTo('secrets');
+    }
+
+});
