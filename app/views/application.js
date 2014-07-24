104a7ef1a3080fc9320679fb5b302f31f346e1c7
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-07-23 20:12:23 -0700

    Scroll to top on mobile

--- app/views/application.js
+++ app/views/application.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-    classNames: ['full-height']
+    classNames: ['full-height'],
+
+    didInsertElement: function () {
+        var isMobile = /mobi/i.test(window.navigator.userAgent);
+        if (isMobile && !window.location.hash && !window.pageYOffset) {
+            window.setTimeout(function () {
+                window.scrollTo(0, 1);
+            }, 0);
+        }
+    }
 });
