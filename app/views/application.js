273ebb027ee639cd226fed4bf93dc43a2cfc8fc5
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-07-24 19:57:06 -0700

    Scroll to top will not work without full screen support

--- app/views/application.js
+++ app/views/application.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-    classNames: ['full-height'],
-
-    didInsertElement: function () {
-        var isMobile = /mobi/i.test(window.navigator.userAgent);
-        if (isMobile && !window.location.hash && !window.pageYOffset) {
-            window.setTimeout(function () {
-                window.scrollTo(0, 1);
-            }, 0);
-        }
-    }
+    classNames: ['full-height']
 });
