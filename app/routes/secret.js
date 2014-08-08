1ad8b1999ea853a913705a606bb9b270bea0a4ea
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-08-07 20:13:38 -0700

    Simplify the secret route by removing the name from the only outlet in the secrets template

--- app/routes/secret.js
+++ app/routes/secret.js
@@ -1,9 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-
-    renderTemplate: function () {
-        this.render({outlet: 'secret'});
-    }
-
-});
+export default Ember.Route.extend({});
