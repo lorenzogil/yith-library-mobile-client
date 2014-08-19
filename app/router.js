b2977190c693d29adc61abce6359fa5ac5a55e0e
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-08-19 15:39:23 -0700

    queryParams are specified in the controller, not the router

--- app/router.js
+++ app/router.js
@@ -6,10 +6,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
     this.route('firstTime', {path: '/first-time'});
-    this.resource('secrets', {
-        path: '/secrets',
-        queryParams: ['tag']
-    }, function () {
+    this.resource('secrets', {path: '/secrets'}, function () {
         this.resource('secret', {path: '/:secret_id'});
         this.route('tags', {path: '/tags'});
     });
